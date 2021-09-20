36750c4681ebaf9b6ac045662f508d9b239cfe21
Bruno B. Miranda <bruno.b.miranda@hotmail.com.br>
2021-09-20 09:08:19 -0300

    1º Bimestre

--- aula16-arrowFunctions/callback.js
+++ aula16-arrowFunctions/callback.js
@@ -1,7 +1,37 @@
-const somar = (numeroA, numeroB) => numeroA + numeroB;
+/* const somar = (numeroA, numeroB) => numeroA + numeroB;
 
 const calculadora = (numeroA, numeroB, operacao) => operacao(numeroA, numeroB);
 
 
 console.log(calculadora(50, 10, somar));
-console.log(calculadora(100, 25, (numeroA, numeroB) => numeroA - numeroB));
+console.log(calculadora(100, 25, (numeroA, numeroB) => numeroA - numeroB)); */
+
+
+/* function nomeCompleto(nome, sobrenome) {
+  //return nome + " " + sobrenome; 
+  return nome[0] + sobrenome [0];
+};
+
+function bomDia(nome, sobrenome, callback){
+  return "Olá, " + callback(nome,sobrenome);
+};
+
+bomDia("Amanda", "Ferreira", nomeCompleto)
+//Olá, Amanda Ferreira
+console.log(bomDia("Amanda", "Ferreira", nomeCompleto)) */
+
+
+function acaoCarro(callback){
+  return callback();
+}
+
+function andar() {
+  return "o carro está andando";
+}
+
+function parar() {
+  return "o carro parou";
+}
+
+console.log(acaoCarro(andar))
+console.log(acaoCarro(parar))
